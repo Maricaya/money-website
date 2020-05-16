4359af5c80e88f443bd64fbb7e50eb5949266ecc
褚煦露 <chuxulu@lezhixing.com.cn>
2020-05-16 23:36:43 +0800

    deploy

---
+++ precache-manifest.9fe385832ff200900fcb74ff64ef7820.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ce67333fb8fc07b0c7628c654b33f87",
+    "url": "/money-website/index.html"
+  },
+  {
+    "revision": "8bf05f665737a7613e9c",
+    "url": "/money-website/static/css/main.32b2ca63.chunk.css"
+  },
+  {
+    "revision": "ba602fc6e522e0dfe914",
+    "url": "/money-website/static/js/2.20fd8e5d.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/money-website/static/js/2.20fd8e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bf05f665737a7613e9c",
+    "url": "/money-website/static/js/main.9a34b2c3.chunk.js"
+  },
+  {
+    "revision": "7b60ed14f73d9bff7d37",
+    "url": "/money-website/static/js/runtime-main.7c431645.js"
+  }
+]);
